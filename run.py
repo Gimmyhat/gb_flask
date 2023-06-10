05118aed03e6d6741a1b01b3a60760986d7e4112
Gimmyhat <nikitchenko.as@gmail.com>
2023-06-10 11:06:02 +0800

    подготовка к деплою

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from gb_web.app import create_app
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(
         host="0.0.0.0",
-        port=8000,)
+        port=8000, )
