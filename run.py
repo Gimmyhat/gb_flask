d47d5ea87b10729345e1907ed77fe730b9083ebc
Gimmyhat <nikitchenko.as@gmail.com>
2023-06-10 11:09:31 +0800

    подготовка к деплою

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
-from gb_web.app import create_app
+from werkzeug.security import generate_password_hash
+
+from gb_web.app import create_app, db
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=8000, )
+
+@app.cli.command("init-db", help="create all db")
+def init_db():
+    db.create_all()
+
+
+@app.cli.command("create-users", help="create users")
+def create_users():
+    from gb_web.models import User
+    db.session.add(
+        User(email="name@email.com", password=generate_password_hash("test"))
+    )
+    db.session.commit()
